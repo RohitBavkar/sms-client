7073b4820cf3382e04bc88c9f185239971ea422d
Rohit Bavkar <rohitbavkar95@gmail.com>
2024-12-21 03:44:07 +0530

    css fix

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -44,10 +44,10 @@ const FormModal = ({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
     type === "create"
-      ? "bg-lamaYellow"
+      ? "bg-smsYellow"
       : type === "update"
-      ? "bg-lamaSky"
-      : "bg-lamaPurple";
+      ? "bg-smsSky"
+      : "bg-smsPurple";
 
   const [open, setOpen] = useState(false);
 
