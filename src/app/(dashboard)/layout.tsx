2b296eb44c0a9c899555afb63d7ab9ed0742ae09
Rohit Bavkar <rohitbavkar95@gmail.com>
2024-12-20 06:23:34 +0530

    Parent Teacher and Student pages

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({
       </div>
 
       {/* Right Part */}
-      <div className="w-[85%] md:w-[90%] lg:w-[85%] xl:w-[85%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[85%] md:w-[90%] lg:w-[85%] xl:w-[85%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
